e80375209d25dbe3f6e980d958cd80fb4c7ccffe
Atul Anand <atulcleaver@gmail.com>
2021-02-12 13:14:15 +0530

    new improvments made.

---
+++ MadLibs.py
@@ -0,0 +1,18 @@
+# This the improved mad libs game:-
+
+proper_Noun = input("Give a proper noun: ")
+quantity = input("Give me a quantity: ")
+verb = input("Give me a verb: ")
+adjective = input("Give me an adjective: ")
+adjective2 = input("Give me an another adjective: ")
+adjective3 = input("Give me an another adjective: ")
+noun = input("Give me a noun: ")
+adverb = input("Give me an adverb: ")
+plural = input("Give me a plural noun: ")
+
+print(f"My name is {proper_Noun} and I've known Meaghan for {quantity} years. "
+      f"\nI am so {verb} she is having a baby! She will make a {adjective} mother \nbecause she is {adjective2} and {adjective3}."
+      f"\nThere is no doubt that her little baby will have the most {adjective2}..."
+      f"\nMy best advice?"
+      f"\nDon't forget to {verb} your {noun} everyday. Meaghan, babies need {plural} and to {verb} when they are first born, "
+      f"\n and this will make you {adverb} {verb}. But, most of all, be sure to {verb} and {verb} the baby's {noun}. I wish you, Ben, and your {adjective} baby a lifetime of {adverb} {noun}")
